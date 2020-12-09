6270e19d62d317da02ccab8984921c5fbf3b88dd
kryakushin666 <thebesthd123@gmail.com>
2020-12-09 16:20:36 +0400

    Промежуточная, багнутая версия. Перенес все drawable с png на xml Начал менять экраны с activity на fragment

--- app/src/main/java/com/example/myapplication/MapsActivity.kt
+++ app/src/main/java/com/example/myapplication/MapsActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication
 
 import android.Manifest
-import android.content.DialogInterface
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.VectorDrawable
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -13,6 +16,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -51,8 +55,78 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val homeLatLng = LatLng(latitude, longitude)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        map.addMarker(
+                MarkerOptions()
+                        .position(LatLng(56.84383886160861, 53.191198130527944))
+                        .title("Главный корпус оружейного завода")
+                        .icon(getBitmapDescriptor(R.drawable.ic_bar))
+        )
+        map.addMarker(
+                MarkerOptions()
+                        .position(LatLng(56.844568122459364, 53.191131214863674))
+                        .title("Памятник Дерябину")
+                        .icon(getBitmapDescriptor(R.drawable.ic_bar))
+        )
+        map.addMarker(
+                MarkerOptions()
+                        .position(LatLng(56.848892795955905, 53.19585937814813))
+                        .title("Ижевский индустриальный техникум")
+                        .icon(getBitmapDescriptor(R.drawable.ic_bar))
+        )
+        map.addMarker(
+                MarkerOptions()
+                        .position(LatLng(56.84408400157632, 53.19771856787305))
+                        .title("Памятник ижевским оружейникам")
+                        .icon(getBitmapDescriptor(R.drawable.ic_bar))
+        )
+        map.addMarker(
+                MarkerOptions()
+                        .position(LatLng(56.84398424917061, 53.198120889542295))
+                        .title("Музей ИЖМАШ")
+                        .icon(getBitmapDescriptor(R.drawable.ic_bar))
+        )
+        map.addMarker(
+                MarkerOptions()
+                        .position(LatLng(56.83996208388173, 53.19589266822729))
+                        .title("Долгий мост и завод «Ижсталь")
+                        .icon(getBitmapDescriptor(R.drawable.ic_bar))
+        )
+        map.addMarker(
+                MarkerOptions()
+                        .position(LatLng(56.85177628926549, 53.2002482478798))
+                        .title("Здание из красного кирпич")
+                        .icon(getBitmapDescriptor(R.drawable.ic_bar))
+        )
+        map.addMarker(
+                MarkerOptions()
+                        .position(LatLng(56.85073186241447, 53.20672264326064))
+                        .title("Музейно-выставочный комплекс стрелкового оружия имени Михаила Тимофеевича Калашникова")
+                        .icon(getBitmapDescriptor(R.drawable.ic_bar))
+        )
+        map.addMarker(
+                MarkerOptions()
+                        .position(LatLng(56.85285289473385, 53.215664171778975))
+                        .title("Арсенал")
+                        .icon(getBitmapDescriptor(R.drawable.ic_bar))
+        )
         enableMyLocation()
         setPoiClick(map)
+        map.setMinZoomPreference(11.5f)
+    }
+
+    private fun getBitmapDescriptor(id: Int): BitmapDescriptor? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val vectorDrawable = getDrawable(id) as VectorDrawable?
+            val h = vectorDrawable!!.intrinsicHeight
+            val w = vectorDrawable.intrinsicWidth
+            vectorDrawable.setBounds(0, 0, w, h)
+            val bm = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bm)
+            vectorDrawable.draw(canvas)
+            BitmapDescriptorFactory.fromBitmap(bm)
+        } else {
+            BitmapDescriptorFactory.fromResource(id)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -89,7 +163,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
     private fun enableMyLocation() {
         if (isPermissionGranted()) {
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
                 //    ActivityCompat#requestPermissions
                 // here to request the missing permissions, and then overriding
@@ -126,7 +200,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                             .position(poi.latLng)
                             .title(poi.name)
                             //.alpha(0.0f)
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon))
+                            .icon(getBitmapDescriptor(R.drawable.ic_icon))
             )
             poiMarker.showInfoWindow()
         }
