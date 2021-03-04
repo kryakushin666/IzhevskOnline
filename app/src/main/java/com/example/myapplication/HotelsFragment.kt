d3b8bb27c42b662bcfb1a3d4e1dd983f8fbcfc54
thebesthd <thebesthd123@gmail.com>
2021-03-04 16:24:53 +0400

    На экраны(Кафе,Музеи,Отели,Парки) добавлены картинки

--- app/src/main/java/com/example/myapplication/HotelsFragment.kt
+++ app/src/main/java/com/example/myapplication/HotelsFragment.kt
@@ -9,33 +9,29 @@ import androidx.navigation.fragment.NavHostFragment
 import kotlinx.android.synthetic.main.fragment_blank.view.*
 import kotlinx.android.synthetic.main.fragment_item.view.*
 
-class BlankFragment : Fragment() {
+/**
+ * A fragment representing a list of Items.
+ */
+class HotelsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
-
         }
-
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val fragmentLayout = inflater.inflate(R.layout.fragment_blank, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val fragmentLayout = inflater.inflate(R.layout.fragment_hotels, container, false)
 
         // получаем ссылку на NavController (навигационный контроллер)
         val navController = NavHostFragment.findNavController(this)
 
         // слушатели кнопок, которые передают адрес навигационному контроллеру
-        fragmentLayout.button.setOnClickListener { navController.navigate(R.id.itemFragment) }
-        fragmentLayout.button1.setOnClickListener { navController.navigate(R.id.parksFragment) }
-        fragmentLayout.button2.setOnClickListener { navController.navigate(R.id.hotelsFragment) }
-        fragmentLayout.button3.setOnClickListener { navController.navigate(R.id.cafeFragment) }
+        fragmentLayout.buttons.setOnClickListener { navController.navigate(R.id.navigation_home) }
 
         // возвращаем макет фрагмента
         return fragmentLayout
     }
-
 }
